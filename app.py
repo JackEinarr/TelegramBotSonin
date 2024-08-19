c6aaacea4afa0b0988ae09d4078fe617a4117e55
София <samsonova1102@mail.ru>
2024-08-19 21:10:34 +0300

    Start bot and send message

---
+++ app.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import CommandStart
+from aiogram.methods.send_message import SendMessage
+
+bot = Bot(token='7486884923:AAF_tom9VlE6gHPu0yR96xK_kKBpStCHisY', default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: types.Message) -> None:
+    await message.answer("Привет!")
+
+
+@dp.message()
+async def message_handler(message: types.Message) -> None:
+    #await SendMessage(chat_id=message.from_user.id, text=message.text)
+    await message.answer(text=message.text)
