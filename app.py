cdc56a23e6057c4d8e282657780bb44ddd8f01b7
Sofiavss <samsonova1102@mail.ru>
2024-08-22 20:20:41 +0300

    добавила другие варианты ответа

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters import CommandStart
-from aiogram.methods.send_message import SendMessage
 
 bot = Bot(token='7486884923:AAF_tom9VlE6gHPu0yR96xK_kKBpStCHisY', default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
@@ -13,8 +12,35 @@ dp = Dispatcher(storage=MemoryStorage())
 async def command_start_handler(message: types.Message) -> None:
     await message.answer("Привет!")
 
+    @dp.message()
+    async def send_cocktail_recipe(message: types.Message) -> None:
+        if message.text == "Отправь рецепт коктейля":
+            await message.answer(text="""Вот простой рецепт коктейля Мохито, который легко приготовить!
+               Ингридиенты:
+               1. 50мл рома
+               2. Сок 1 лайма 
+               3. 1-2 чайные ложки сахара
+               4. 6-8 листиков свежей мяты.
+               5. Газированная вода
+               7. Газированная вода
+               Наслаждайся!
+               """)
+        elif message.text == "Еще рецепт":
+            await message.answer(text="""Рецепт коктейля Маргарита, который тебе понравится!
+            Ингредиенты:
+            1. 50 мл текилы
+            2. 25 мл апельсинового ликера
+            3. 25 мл сока лайма
+            4. Соль для ободка стакана
+            5. Лёд
+            Пробуй!
+            """)
+        else:
+            await message.answer(text=message.text)
 
-@dp.message()
-async def message_handler(message: types.Message) -> None:
+
+
+#@dp.message()
+#async def message_handler(message: types.Message) -> None:
     #await SendMessage(chat_id=message.from_user.id, text=message.text)
-    await message.answer(text=message.text)
+    #await message.answer(text=message.text)
